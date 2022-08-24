64d10662ca2cc90b40645b406a6e526215c07f41
ahmed.mohadin <ahmed.mohadin@outlook.com>
2022-08-24 15:25:49 +0200

    Updates

---
+++ precache-manifest.292d20e3a41e0afbeddca5492b725fc8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff7000721fb86fa085d7",
+    "url": "/memory-card/static/css/main.8c8151ae.chunk.css"
+  },
+  {
+    "revision": "ff7000721fb86fa085d7",
+    "url": "/memory-card/static/js/main.ff700072.chunk.js"
+  },
+  {
+    "revision": "9e2ec8e2eb850665d042",
+    "url": "/memory-card/static/js/1.9e2ec8e2.chunk.js"
+  },
+  {
+    "revision": "578ba911c50e8f8ac86f",
+    "url": "/memory-card/static/js/runtime~main.578ba911.js"
+  },
+  {
+    "revision": "a6b079f979a5421d3178e1389fca389c",
+    "url": "/memory-card/index.html"
+  }
+];
